10dd2b1b99384d78df763971fe1cdba80855aa2c
Thom Wright <thom.wright@candide.eu>
2018-06-22 14:36:26 +0100

    Add compiled lib dir
    So we can point to this fork without publishing

---
+++ lib/src/core/types.d.ts
@@ -0,0 +1,35 @@
+import { DocumentNode } from 'graphql';
+import { QueryStoreValue } from '../queries/store';
+import { NetworkStatus } from '../queries/networkStatus';
+export declare type QueryListener = (queryStoreValue: QueryStoreValue) => void;
+export declare type PureQueryOptions = {
+  query: DocumentNode;
+  variables?: {
+    [key: string]: any;
+  };
+};
+export declare type ApolloQueryResult<T> = {
+  data: T;
+  loading: boolean;
+  networkStatus: NetworkStatus;
+  stale: boolean;
+};
+export declare type ApolloExecutionResult<
+  T = {
+    [key: string]: any;
+  }
+> = {
+  data?: T;
+};
+export enum FetchType {
+  normal = 1,
+  refetch = 2,
+  poll = 3,
+}
+export interface IdGetterObj extends Object {
+  __typename?: string;
+  id?: string;
+}
+export declare type IdGetter = (
+  value: IdGetterObj,
+) => string | null | undefined;
